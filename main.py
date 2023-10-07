62323d900b0169e69b384ebd445ee16de90c61fb
23536667 <23536667@sun.ac.za>
2023-10-07 22:52:12 +0200

    fix

--- main.py
+++ main.py
@@ -111,4 +111,6 @@ def recommend():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable if available, otherwise default to 5000
+    app.run(host='0.0.0.0', port=port, debug=False)
